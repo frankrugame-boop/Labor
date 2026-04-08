8a4fadb07049a49e72cbbbd909ffbfa4bc4ce495
Stolyrov Ruslan <frank.ru.game@gmail.com>
2026-04-08 23:53:58 +0300

    Связывание логики игры и интерфейса через StatefulWidget

--- lr2/birdle/lib/main.dart
+++ lr2/birdle/lib/main.dart
@@ -48,7 +48,14 @@ class Tile extends StatelessWidget {
 }
 
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
+  GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -56,19 +63,23 @@ class GamePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
           for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (var letter in guess) Tile(letter.char, letter.type),
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
               ],
             ),
           GuessInput(
-            onSubmitGuess: (String guess) {
-              // TODO, handle guess
-              print(guess); // Temporary
+           onSubmitGuess: (String guess) {
+              setState(() { // NEW
+                _game.guess(guess);
+              });
             },
           ),
         ],
@@ -76,6 +87,8 @@ class GamePage extends StatelessWidget {
     );
   }
 }
+
+
 
 
 class GuessInput extends StatelessWidget {
